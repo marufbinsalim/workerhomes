c3cb38920b8b0c4237a930e2e8b6cfc7ed5b7d4b
marufbinsalim <mdmarufbinsalim@gmail.com>
2025-02-22 21:36:32 +0600

    a

--- app/api/send-email/route.js
+++ app/api/send-email/route.js
@@ -63,6 +63,7 @@ export async function POST(req) {
     }
 
     let data = await sgMail.send(msg);
+    console.log(data);
     return NextResponse.json({
       success: true,
       message: "Email sent successfully",
