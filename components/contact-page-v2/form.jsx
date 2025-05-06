75522d33cd7b9c103085ff096bcc10561a4a845c
marufbinsalim <mdmarufbinsalim@gmail.com>
2025-05-06 12:57:46 +0600

    u

--- components/contact-page-v2/form.jsx
+++ components/contact-page-v2/form.jsx
@@ -1,42 +1,48 @@
 export default function ContactForm() {
   return (
-    <div className="">
-      <div className="tw:bg-amber-50 tw:grid tw:grid-cols-2">
+    <div className="font-primary tw:flex tw:flex-col tw:gap-5">
+      <div className="tw:grid tw:grid-cols-2 tw:gap-5">
         <div>
-          <label className="tw:block tw:text-gray-700 tw:mb-1">
+          <label className="tw:block tw:text-[var(--color-font-dark)] tw:mb-[10px] tw:font-semibold">
             First name
           </label>
           <input
             type="text"
-            className="tw:w-full tw:px-3 tw:py-2 tw:border tw:border-gray-300 tw:rounded-md focus:tw:outline-none focus:tw:ring-2 focus:tw:ring-blue-500"
+            className="tw:w-full tw:bg-[var(--color-white-grey)] tw:px-3 tw:py-2 tw:border tw:border-gray-300  focus:tw:outline-none focus:tw:ring-2 focus:tw:ring-blue-500"
           />
         </div>
         <div>
-          <label className="tw:block tw:text-gray-700 tw:mb-1">Last name</label>
+          <label className="tw:block tw:text-[var(--color-font-dark)] tw:mb-[10px] tw:font-semibold">
+            Last name
+          </label>
           <input
             type="text"
-            className="tw:w-full tw:bg-black tw:px-3 tw:py-2 tw:border tw:border-gray-300 tw:rounded-md focus:tw:outline-none focus:tw:ring-2 focus:tw:ring-blue-500"
+            className="tw:w-full tw:bg-[var(--color-white-grey)] tw:px-3 tw:py-2 tw:border tw:border-gray-300  focus:tw:outline-none focus:tw:ring-2 focus:tw:ring-blue-500"
           />
         </div>
       </div>
 
       <div className="tw:mb-4">
-        <label className="tw:block tw:text-gray-700 tw:mb-1">Email</label>
+        <label className="tw:block tw:text-[var(--color-font-dark)] tw:mb-[10px] tw:font-semibold">
+          Email
+        </label>
         <input
           type="email"
-          className="tw:w-full tw:px-3 tw:py-2 tw:border tw:border-gray-300 tw:rounded-md focus:tw:outline-none focus:tw:ring-2 focus:tw:ring-blue-500"
+          className="tw:w-full tw:bg-[var(--color-white-grey)] tw:px-3 tw:py-2 tw:border tw:border-gray-300  focus:tw:outline-none focus:tw:ring-2 focus:tw:ring-blue-500"
         />
       </div>
 
       <div className="tw:mb-6">
-        <label className="tw:block tw:text-gray-700 tw:mb-1">Message</label>
+        <label className="tw:block tw:text-[var(--color-font-dark)] tw:mb-[10px] tw:font-semibold">
+          Message
+        </label>
         <textarea
-          className="tw:w-full tw:px-3 tw:py-2 tw:border tw:border-gray-300 tw:rounded-md focus:tw:outline-none focus:tw:ring-2 focus:tw:ring-blue-500 tw:h-32"
+          className="tw:w-full tw:bg-[var(--color-white-grey)] tw:px-3 tw:py-2 tw:border tw:border-gray-300  focus:tw:outline-none focus:tw:ring-2 focus:tw:ring-blue-500 tw:h-32"
           placeholder="Leave a message for us..."
         ></textarea>
       </div>
 
-      <button className="tw:w-max tw:bg-blue-600  tw:py-2 tw:px-4 tw:rounded-md hover:tw:bg-blue-700 focus:tw:outline-none focus:tw:ring-2 focus:tw:ring-blue-500 focus:tw:ring-offset-2">
+      <button className="tw:w-max tw:bg-blue-600  tw:py-2 tw:px-4  hover:tw:bg-blue-700 focus:tw:outline-none focus:tw:ring-2 focus:tw:ring-blue-500 focus:tw:ring-offset-2">
         Send to a message
       </button>
     </div>
