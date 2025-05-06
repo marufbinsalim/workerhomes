67ae548d8e974e63fdebc194870b7e77962ce7e1
marufbinsalim <mdmarufbinsalim@gmail.com>
2025-05-06 13:00:48 +0600

    p

--- app/[locale]/(test)/test/page.jsx
+++ app/[locale]/(test)/test/page.jsx
@@ -1,9 +1,12 @@
+import Footer from "@/components/common-v2/footer";
+
 export default function Test() {
   return (
     <div>
       <p className="tw:bg-amber-300 tw:border-2 font-primary tw:font-bold">
         Hello World
       </p>
+      <Footer />
     </div>
   );
 }
