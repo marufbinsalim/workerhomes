c929f248138b7b9422f6da50ebf48587f35ca0e3
marufbinsalim <mdmarufbinsalim@gmail.com>
2025-05-09 20:21:45 +0600

    email verified page design completed

--- app/[locale]/(auth)/email-verified/page.jsx
+++ app/[locale]/(auth)/email-verified/page.jsx
@@ -1,46 +1,52 @@
-import Link from '@/components/common/Link'
-import Wrapper from '@/components/layout/Wrapper'
-import { getTranslations } from 'next-intl/server'
-import dynamic from 'next/dynamic'
+import ForgotForm from "@/components/authentication-v2/forgot/forgotForm";
+import Navbar from "@/components/common-v2/nav";
+import Link from "@/components/common/Link";
+import Wrapper from "@/components/layout/Wrapper";
+import { getTranslations } from "next-intl/server";
+import dynamic from "next/dynamic";
 
 export const metadata = {
   title: `Forgot Password || HomesWorkers - Find your best place with lower charges.`,
   description: `HomesWorkers - Find your best place with lower charges.`,
-}
+};
 
 const EmailVerifiedPage = async ({ params }) => {
-  const t = await getTranslations('email-verified')
+  const t = await getTranslations("email-verified");
 
   return (
-    <Wrapper>
-      {/* End Page Title */}
-
-      <div className='header-margin'></div>
-      {/* header top margin */}
-
-      <section className='layout-pt-lg layout-pb-lg bg-blue-2'>
-        <div className='container'>
-          <div className='row justify-center'>
-            <div className='col-xl-6 col-lg-7 col-md-9'>
-              <div className='px-50 py-50 sm:px-20 sm:py-20 bg-white shadow-4 rounded-4 text-center'>
-                <h1>{t('title')}</h1>
-                <p className='my-30'>{t('description')}</p>
-
-                <Link
-                  href='/login'
-                  className='button py-20  -dark-1 bg-blue-1 text-white w-100 mt-30'
-                >
-                  {t('button')}
-                </Link>
-              </div>
+    <div className="tw:pt-[90px] font-primary">
+      <Navbar />
+      <div className="tw:flex tw:flex-col tw:md:flex-row tw:p-4 tw:md:p-20 tw:gap-10">
+        <div className="tw:md:flex-2/3 tw:flex tw:flex-col tw:items-center">
+          <div className="tw:flex tw:flex-col tw:gap-1 tw:mb-2.5 tw:text-center">
+            <h2 className="tw:text-[var(--color-font-dark)] tw:font-medium tw:text-[40px]">
+              Thank You!
+            </h2>
+            <p className="tw:text-[var(--color-font-regular)] tw:m-0">
+              Your email has been verified successfully. You can now
+              <Link href="/login">
+                <span className="tw:text-[var(--color-primary)] tw:hover:underline tw:mx-1">
+                  Log-In
+                </span>
+              </Link>
+              to your account
+            </p>
+          </div>
+          <div className="tw:flex tw:flex-col tw:gap-4 tw:text-center">
+            <div className="">
+              <img
+                src="/assets/login-illustration.png"
+                alt="forgot illustration"
+                className="tw:w-full tw:m-auto tw:my-2.5"
+              />
             </div>
           </div>
         </div>
-      </section>
-    </Wrapper>
-  )
-}
+      </div>
+    </div>
+  );
+};
 
 export default dynamic(() => Promise.resolve(EmailVerifiedPage), {
   ssr: false,
-})
+});
