c92d7d2f7a8d08d1f5386a84d936054c6c2a8263
marufbinsalim <mdmarufbinsalim@gmail.com>
2025-05-25 02:56:44 +0600

    u

--- components/pages/profile/index.jsx
+++ components/pages/profile/index.jsx
@@ -13,10 +13,11 @@ import { url } from "@/config";
 import useFetch from "@/hooks/useFetch";
 import { remove } from "@/lib/services/user";
 import axios from "axios";
-import { CircleDashed } from "lucide-react";
+import { CircleDashed, Edit, Lock } from "lucide-react";
 import { signOut, useSession } from "next-auth/react";
 import { useTranslations } from "next-intl";
 import { useState } from "react";
+import { FaToggleOff, FaToggleOn } from "react-icons/fa";
 
 const ProfilePage = ({ locale }) => {
   const t = useTranslations("profile");
@@ -42,6 +43,8 @@ const ProfilePage = ({ locale }) => {
       populate: ["address", "subscriptions"],
     },
   });
+
+  const [isBusiness, setIsBusiness] = useState(false);
 
   const handleDelete = async () => {
     setLoading(true);
@@ -72,9 +75,85 @@ const ProfilePage = ({ locale }) => {
   };
 
   return (
-    <div className="tw:max-h-[calc(100dvh-100px)] tw:overflow-y-auto overflow-x-hidden tw:mx-4 tw:mt-[70px]">
-      <div className="tw:py-8 tw:rounded-lg tw:bg-white tw:shadow-md tw:px-10">
+    <div className="tw:max-h-[calc(100dvh-100px)] tw:overflow-y-auto overflow-x-hidden tw:md:mx-4 tw:mt-[70px]">
+      {/* basic information card */}
+      <div className="tw:py-4 tw:md:py-8 tw:rounded-lg tw:bg-white tw:shadow-md tw:px-4 tw:md:px-10">
         <>
+          {isBusiness === false && (
+            <div className="tw:border tw:border-[#D8E0ED] tw:px-3 tw:md:px-5 tw:py-4 tw:md:py-7 tw:w-full">
+              <h2 className="tw:text-lg tw:font-semibold tw:mb-4">
+                Basic Information
+              </h2>
+              {/* Toggle Switch */}
+              <div className="tw:flex tw:items-center tw:mb-6">
+                <FaToggleOff
+                  className="tw:text-gray-500 tw:w-6 tw:h-6"
+                  onClick={() => setIsBusiness(!isBusiness)}
+                />
+                <label className="tw:ml-2 tw:text-sm">
+                  Turn on business account
+                </label>
+              </div>
+              {/* User Info Grid */}
+              <div className="tw:grid tw:grid-cols-1 tw:md:grid-cols-2 tw:gap-y-4 tw:gap-x-8 tw:mb-6">
+                <div>
+                  <p className="tw:text-sm tw:text-gray-500">First Name</p>
+                  <p className="tw:text-base">{data?.first_name || ""}</p>
+                </div>
+                <div>
+                  <p className="tw:text-sm tw:text-gray-500">Last Name</p>
+                  <p className="tw:text-base">{data?.last_name || ""}</p>
+                </div>
+                <div>
+                  <p className="tw:text-sm tw:text-gray-500">Email</p>
+                  <p className="tw:text-base">abrahamlincoln@gmail.com</p>
+                </div>
+                <div>
+                  <p className="tw:text-sm tw:text-gray-500">Phone No.</p>
+                  <p className="tw:text-base">{data?.phone || ""}</p>
+                </div>
+                <div>
+                  <p className="tw:text-sm tw:text-gray-500">
+                    Street & House No.
+                  </p>
+                  <p className="tw:text-base">D block, road 8, house</p>
+                </div>
+                <div>
+                  <p className="tw:text-sm tw:text-gray-500">City</p>
+                  <p className="tw:text-base">{data?.address?.city || ""}</p>
+                </div>
+                <div>
+                  <p className="tw:text-sm tw:text-gray-500">Country</p>
+                  <p className="tw:text-base">{data?.address?.country || ""}</p>
+                </div>
+                <div>
+                  <p className="tw:text-sm tw:text-gray-500">Postal Code</p>
+                  <p className="tw:text-base">
+                    {data?.address?.zip_code || ""}
+                  </p>
+                </div>
+                <div>
+                  <p className="tw:text-sm tw:text-gray-500">Languages</p>
+                  <p className="tw:text-base">
+                    {data?.locale?.toUpperCase() || ""}
+                  </p>
+                </div>
+              </div>
+
+              <div className="tw:flex tw:gap-3">
+                <button className="tw:flex tw:items-center tw:bg-orange-500 tw:text-white tw:px-4 tw:py-2 tw:rounded tw:text-sm tw:hover:bg-orange-600 tw:transition">
+                  <Edit className="tw:w-4 tw:h-4 tw:mr-2" />
+                  Edit
+                </button>
+                <button className="tw:flex tw:items-center tw:bg-white tw:text-gray-700 tw:border tw:border-gray-300 tw:px-4 tw:py-2 tw:rounded tw:text-sm tw:hover:bg-gray-100 tw:transition">
+                  <Lock className="tw:w-4 tw:h-4 tw:mr-2" />
+                  Change Password
+                </button>
+              </div>
+            </div>
+          )}
+
+          <pre>{JSON.stringify(data, null, 2)}</pre>
           <ProfileForm formData={data} onSuccess={() => signOut()} />
 
           <Divider side="center" title={t("form.field.payment-methods")} />
